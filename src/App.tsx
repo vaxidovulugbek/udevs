d3f56dcdc3192b3b5dccd2457f1c0c36591deaf4
ulugbek <vulugb@gmail.com>
2024-08-15 09:38:30 +0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import "./App.css";
 import { AppProps } from "types/interface";
+import { ScrollTop } from "helpers/ScrollToTop";
 
 function App({ children }: AppProps) {
-	return <div className="App">{children}</div>;
+	return (
+		<div className="App">
+			{children}
+			<ScrollTop />
+		</div>
+	);
 }
 
 export default App;
